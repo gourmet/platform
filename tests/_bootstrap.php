4ae747fb0dc1f9a1415e4b9fe0ac159e440531bb
Jad BITAR <jad@Jads-Mac-Pro.local>
2014-07-18 18:19:09 -0400

    Add cake fixtures to codeception.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 // This is global bootstrap for autoloading
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap' . DIRECTORY_SEPARATOR . 'start.php';
+require '_helpers' . DS . 'Cake3' . DS . 'FixtureInjector.php';
 
 disableDebug();

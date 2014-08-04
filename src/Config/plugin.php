1179d606e81bdcfac1333fac1b259aa5bcc998d0
Jad BITAR <jad@Jads-Mac-Pro.local>
2014-08-04 15:44:08 -0400

    Load whoops plugin by default.

--- src/Config/plugin.php
+++ src/Config/plugin.php
@@ -4,3 +4,4 @@ use Cake\Core\Plugin;
 
 Plugin::load('Crud');
 Plugin::load('Robo', ['namespace' => 'Gourmet\\Robo', 'autoload' => true]);
+Plugin::load('Whoops', ['namespace' => 'Gourmet\\Whoops', 'autoload' => true]);

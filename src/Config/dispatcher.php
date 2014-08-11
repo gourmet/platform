b180a07ec5b9a60f3a48381c91f345c7519da900
Jad BITAR <jad@Jads-Mac-Pro.local>
2014-08-11 12:05:03 -0400

    Remove the CacheFilter middleware.

--- src/Config/dispatcher.php
+++ src/Config/dispatcher.php
@@ -6,6 +6,5 @@ use Cake\Routing\DispatcherFactory;
  * Connect middleware/dispatcher filters.
  */
 DispatcherFactory::add('Asset');
-DispatcherFactory::add('Cache');
 DispatcherFactory::add('Routing');
 DispatcherFactory::add('ControllerFactory');

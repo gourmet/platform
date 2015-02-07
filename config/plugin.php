3b9f162abf97d635f48c032988a6cf898764e95e
Jad Bitar <jadbitar@mac.com>
2015-02-07 10:58:50 -0500

    Conditionally loadoad debug_kit

--- config/plugin.php
+++ config/plugin.php
@@ -1,11 +1,22 @@
 <?php
 
+use Cake\Core\Configure;
+use Cake\Core\Exception\MissingPluginException;
 use Cake\Core\Plugin;
 
 Plugin::load('AssetCompress', ['bootstrap' => true]);
-Plugin::load('DebugKit', ['bootstrap' => true]);
 Plugin::load('Crud');
 Plugin::load('Migrations');
 Plugin::load('Gourmet/Faker');
 Plugin::load('Gourmet/Robo');
 Plugin::load('Gourmet/Whoops');
+
+/**
+ * If in debug mode, try loading DebugKit.
+ */
+if (Configure::read('debug')) {
+    try {
+        Plugin::load('DebugKit', ['bootstrap' => true]);
+    } catch (MissingPluginException $e) {
+    }
+}

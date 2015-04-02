dc01747362b6d5e49f92a89dbb9f15036bce6639
Jad Bitar <jadbitar@mac.com>
2015-04-02 17:05:12 -0400

    Follow cakephp/app@8ad2df1572c04d5925a90fbc0e37e6a8964064ea

--- bin/cake.php
+++ bin/cake.php
@@ -17,6 +17,19 @@
  * @since         2.0.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-include dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap' . DIRECTORY_SEPARATOR . 'start.php';
+
+$minVersion = '5.4.16';
+if (file_exists('composer.json')) {
+    $composer = json_decode(file_get_contents('composer.json'));
+    if (isset($composer->require->php)) {
+        $minVersion = preg_replace('/([^0-9\.])/', '', $composer->require->php);
+    }
+}
+if (version_compare(phpversion(), $minVersion, '<')) {
+    fwrite(STDERR, sprintf("Minimum PHP version: %s. You are using: %s.\n", $minVersion, phpversion()));
+    exit(-1);
+}
+
+include dirname(__DIR__) . '/config/bootstrap.php';
 
 exit(Cake\Console\ShellDispatcher::run($argv));

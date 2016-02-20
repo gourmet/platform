eba5c59758f953380bea1ca2fee2484b1653fe4c
Jad Bitar <jadbitar@mac.com>
2016-02-20 10:53:04 -0500

    Added the asset compress helper

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -6,4 +6,12 @@ use Cake\View\View;
 class AppView extends View
 {
 
+    /**
+     * Helpers.
+     *
+     * @var array
+     */
+    public $helpers = [
+        'AssetCompress.AssetCompress',
+    ];
 }

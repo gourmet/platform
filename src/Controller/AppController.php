b3a5875e1ad8e9e9f06e01f31562de2ab947db53
Jad Bitar <jadbitar@mac.com>
2016-02-19 19:53:21 -0500

    Improved CRUD basic setup

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,10 +1,84 @@
 <?php
-
 namespace App\Controller;
 
 use Cake\Controller\Controller;
+use Crud\Controller\ControllerTrait;
 
 class AppController extends Controller
 {
+    use ControllerTrait;
 
+    /**
+     * Tells if the CRUD listeners should be enabled.
+     *
+     * @var bool
+     */
+    public $isCrudEnabled = true;
+
+    /**
+     * Tells if CRUD view listener and view should be used.
+     *
+     * @var bool
+     */
+    public $isCrudViewEnabled = true;
+
+    /**
+     * Tells if the CRUD API listeners should be enabled.
+     *
+     * @var bool
+     */
+    public $isCrudApiEnabled = false;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('Flash');
+        $this->loadComponent('RequestHandler');
+
+        $this->_setupCrud();
+    }
+
+    protected function _setupCrud()
+    {
+        if (!$this->isCrudEnabled) {
+            return;
+        }
+
+        $this->loadComponent('Crud.Crud', [
+            'listeners' => [
+                'Crud.Redirect',
+                'Crud.RelatedModels',
+            ]
+        ]);
+
+        $this->_setupCrudView();
+        $this->_setupCrudApi();
+    }
+
+    protected function _setupCrudView()
+    {
+        if (!$this->isCrudViewEnabled) {
+            return;
+        }
+
+        $this->Crud->addListener('CrudView.View');
+        $this->viewBuilder()
+            ->className('CrudView.Crud')
+            ->layout('default');
+    }
+
+    protected function _setupCrudApi()
+    {
+        if (!$this->isCrudApiEnabled) {
+            return;
+        }
+
+        $this->Crud->addListener('Crud.Api');
+        $this->Crud->addListener('Crud.ApiPagination');
+        $this->Crud->addListener('Crud.ApiQueryLog');
+    }
 }
--- src/View/AppView.php
+++ src/View/AppView.php
@@ -1,43 +1,7 @@
 <?php
 namespace App\View;
 
-use Cake\View\View;
-
 class AppView extends View
 {
-    /**
-     * {@inheritdoc}
-     *
-     * @return void
-     */
-    public function initialize()
-    {
-        $this->_setupAssetCompress();
-        $this->_setupBootstrapUI();
-    }
 
-    /**
-     * Load the AssetCompress helper.
-     *
-     * @return void
-     */
-    protected function _setupAssetCompress()
-    {
-        $this->loadHelper('AssetCompress.AssetCompress');
-    }
-
-    /**
-     * Load the BootstrapUI helpers.
-     *
-     * @return void
-     */
-    protected function _setupBootstrapUI()
-    {
-        $helpers = ['Flash', 'Form'];
-        foreach ($helpers as $helper) {
-            $this->loadHelper($helper, [
-                'className' => 'BootstrapUI.' . $helper,
-            ]);
-        }
-    }
 }
